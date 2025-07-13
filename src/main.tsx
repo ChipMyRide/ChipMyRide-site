4bbf88e8a957da7629c0a8cf9fd56185a6f18da9
ChipMyRide <damonakers08@gmail.com>
2025-07-13 11:22:37 +0200

    Delete src/main.tsx

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div className="text-white text-center mt-10 text-3xl">ChipMyRide läuft!</div>
-  </React.StrictMode>,
-)
