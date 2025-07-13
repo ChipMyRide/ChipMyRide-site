2279a7259f609f6ee5dc71ff92642702aa91500f
ChipMyRide <damonakers08@gmail.com>
2025-07-13 13:18:51 +0200

    Update ChipMyRidePage.tsx

--- src/ChipMyRidePage.tsx
+++ src/ChipMyRidePage.tsx
@@ -12,7 +12,7 @@ export default function ChipMyRidePage() {
         <img
           src="/logo.png"
           alt="Chip My Ride Banner"
-          className="h-[70%] object-contain"
+          className="h-[100%] object-contain"
         />
       </header>
 
