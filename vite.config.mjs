fe49e2e4606fd41a6307fe7d75bc6115742cc4b2
ChipMyRide <damonakers08@gmail.com>
2025-07-14 12:55:14 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,26 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+/** @type {import('vite').UserConfig} */
+export default {
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  },
   server: {
     fs: {
       allow: ['.'],
     },
   },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  preview: {
-    port: 4173,
-    open: true
-  }
-});
+};
