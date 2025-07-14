1064bd02711c1a0641e2f32121e85fd0819e6d1c
ChipMyRide <damonakers08@gmail.com>
2025-07-14 10:58:29 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import App from './App';
 import './index.css';
-import ChipMyRidePage from './ChipMyRidePage';
-import Stage1InfoPage from './Stage1InfoPage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ChipMyRidePage />} />
-        <Route path="/tuning/stage1" element={<Stage1InfoPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
