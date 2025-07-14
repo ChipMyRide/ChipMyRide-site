d9a8ca3e8bba3b853173015954e8f42c00c2ff9e
ChipMyRide <damonakers08@gmail.com>
2025-07-14 11:57:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <—— das sorgt dafür, dass Vite im Root served
+  server: {
+    fs: {
+      allow: ['.'],
+    }
+  }
 });
