283bf6d48a432adf4ace48371c1a42bf434d8736
ChipMyRide <damonakers08@gmail.com>
2025-07-15 02:17:24 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,7 @@
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-/** @type {import('vite').UserConfig} */
-export default {
+export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: ['.'],
-    },
-  },
-};
+  base: '/', // <-- unbedingt so lassen bei Vercel Root Deployment
+})
